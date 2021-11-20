1e4494343ac8c2efbd146c65d357bbecac11c661
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2021-11-21 01:27:54 +0200

    added basic edge types

--- treebuilder.py
+++ treebuilder.py
@@ -7,10 +7,13 @@ import matplotlib.pyplot as plt
 address_mapper = {}
 
 def parse(lines):
+    data_edge_type = 0
+    control_edge_type = 1
+    instruction_edge_type = 2
     classes = []
     instructions = []
     functions = []
-    graph = nx.Graph()
+    graph = nx.DiGraph()
     idx = 0
     for line in lines:
         if line:
@@ -30,24 +33,24 @@ def parse(lines):
                 if('(' in line):
                     args = line[line.find('(')+1: line.find(')')].split(',')
                 if(functionName):
-                    graph.add_edge(idx, classes[-1])
+                    graph.add_edge(classes[-1], idx, e_type=control_edge_type)
                     f_idx = idx
                     idx += 1
 
                 type = re.findall("(public|private|protected)",line)
                 if type:
-                    typeNode = graph.add_edge(f_idx, idx)
+                    graph.add_edge(f_idx, idx, e_type=instruction_edge_type)
                     idx += 1
 
                 if (functionName): 
                     returnType = re.findall("(?<=\s)(.*?)(?=\s{1,}%s)"%functionName,line)
                     if returnType:
-                        graph.add_edge(f_idx, idx)
+                        graph.add_edge(f_idx, idx, e_type=instruction_edge_type)
                         idx += 1
                 
                 for arg in args:
                     if arg:
-                        graph.add_edge(f_idx, idx)
+                        graph.add_edge(f_idx, idx, e_type=instruction_edge_type)
                         idx += 1
                 functions.append(f_idx)
 
@@ -57,24 +60,24 @@ def parse(lines):
                 instructionName = instructionInfo[1]
                 I_idx = idx
                 if(instructions):
-                    graph.add_edge(instructions[-1], idx)
+                    graph.add_edge(instructions[-1], idx, e_type=control_edge_type)
                     idx += 1
                 else:
-                    graph.add_edge(functions[-1], idx)
+                    graph.add_edge(functions[-1], idx, e_type=control_edge_type)
                     idx += 1
                 if(len(instructionInfo) > 2):
                     if(re.findall("#{0,1}\d+", instructionInfo[2])):
-                        graph.add_edge(I_idx, idx)
+                        graph.add_edge(idx, I_idx, e_type=instruction_edge_type)
                         idx += 1
                         if(instructionInfo[2][0] == '#'):
                             comment = re.findall("(?<=\/\/ ).*[^;]", line)[0]
                             address_mapper[instructionInfo[2]] = comment
                 if(len(instructionInfo) > 3):
                     if(re.findall("#{0,1}\d+", instructionInfo[3])):
-                        graph.add_edge(I_idx, idx)
+                        graph.add_edge(idx, I_idx, e_type=instruction_edge_type)
                         idx += 1
                 instructions.append(I_idx)
-    return graph    
+    return dgl.from_networkx(graph, edge_attrs=['e_type'])
 
 
 if __name__ == "__main__":
@@ -113,8 +116,8 @@ if __name__ == "__main__":
        
     
     graph = parse(lines)
-    nx.draw(graph, with_labels=True)
-    plt.show()
+    # nx.draw(graph, with_labels=True)
+    # plt.show()
     # dgl_g = dgl.from_networkx(graph)
 
     print(graph)
