4cb4fd593853d6bc6036c987d22a46a77dc8ba6b
botta633 <bottaawesome633@gmail.com>
2021-12-02 19:58:15 +0200

    Update treebuilder.py

--- treebuilder.py
+++ treebuilder.py
@@ -31,29 +31,29 @@ def parse(lines):
                 if('(' in line):
                     args = line[line.find('(')+1: line.find(')')].split(',')
                 if(functionName):
-                    graph.add_edge(classes[-1], idx, e_type=control_edge_type)
+                    graph.add_edge(classes[-1], idx, e_type=control_edge_type, n_type = -2)
 
                     f_idx = idx
                     idx += 1
 
                 type = re.findall("(public|private|protected)",line)
                 if type:
-                    graph.add_edge(f_idx, idx, e_type=instruction_edge_type)
+                    graph.add_edge(f_idx, idx, e_type=instruction_edge_type, n_type = -2)
                     idx += 1
 
                 if (functionName): 
                     returnType = re.findall("(?<=\s)(.*?)(?=\s{1,}%s)"%functionName,line)
                     if returnType:
-                        graph.add_edge(f_idx, idx, e_type=instruction_edge_type)
+                        graph.add_edge(f_idx, idx, e_type=instruction_edge_type, n_type = -2)
                         idx += 1
                 
                 for arg in args:
                     if arg:
-                        graph.add_edge(f_idx, idx, e_type=instruction_edge_type)
+                        graph.add_edge(f_idx, idx, e_type=instruction_edge_type, n_type = -2)
                         idx += 1
                 functions.append(f_idx)
-
-            #instructions
+                     
+       #instructions
             elif re.findall("\d+[:]\s\w+",line):
                 instructionInfo = re.findall("[^\s\\:\\\\\/\/<>.\'\"(),;]\w{0,}",line)
                 instructionName = instructionInfo[1]
@@ -68,22 +68,24 @@ def parse(lines):
                     graph.add_edge(instructions[-1], idx, e_type=control_edge_type, n_type = identifier)
                     idx += 1
                 else:
-                    graph.add_edge(functions[-1], id, e_type=control_edge_type, n_type = identifier)
+                    graph.add_edge(functions[-1], idx, e_type=control_edge_type, n_type = identifier)
                     idx += 1
                 if(len(instructionInfo) > 2):
                     if(re.findall("#{0,1}\d+", instructionInfo[2])):
-                        graph.add_edge(idx, I_idx, e_type=instruction_edge_type)
+                        graph.add_edge(idx, I_idx, e_type=instruction_edge_type, n_type = 0)
                         idx += 1
                         if(instructionInfo[2][0] == '#'):
                             if instructionInfo[2] in sameloc_mapper:
-                                graph.add_edge(sameloc_mapper[instructionInfo[2]], idx, e_type = control_edge_type)
+                                graph.add_edge(sameloc_mapper[instructionInfo[2]], idx, e_type = control_edge_type, n_type = -1)
                             
                             sameloc_mapper[instructionInfo[2]] = idx
                             comment = re.findall("(?<=\/\/ ).*[^;]", line)[0]
                             address_mapper[instructionInfo[2]] = comment
                 if(len(instructionInfo) > 3):
                     if(re.findall("#{0,1}\d+", instructionInfo[3])):
-                        graph.add_edge(idx, I_idx, e_type=instruction_edge_type)
+                        graph.add_edge(idx, I_idx, e_type=instruction_edge_type, n_type = -1)
                         idx += 1
                 instructions.append(I_idx)
-    return graph
+
+    return dgl.from_networkx(graph, edge_attrs = ['e_type','n_type'])
+    
