d52fe0d07813c3d9ad26e6e8c26b922630c3cc53
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2022-02-26 21:58:26 +0200

    data_proc class

--- cavaj/src/data_proc.py
+++ cavaj/src/data_proc.py
@@ -1,8 +1,24 @@
 from tqdm import tqdm
 from pathlib import Path
+import networkx as nx
+import dgl
 
-def load_src(path):
-	for i in tqdm(sorted())
+class data_proc:
 
-def load_llc():
-	pass
+	def __init__(self):
+		self.exclude = set()
+		self.trg = []
+
+	def load_src(self, path):
+		for i in tqdm(sorted(path.iterdir())):
+			with open(i) as file:
+				try:
+					self.trg.append(self.proc_ast(file))
+				except:
+					self.exclude.add(i)
+
+	def load_llc(self):
+		pass
+
+	def __proc_ast(in_file):
+		pass
