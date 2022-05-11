9f50a81859ff27a3c9b7e1c56ab231e166ea4510
ThesisGroup7 <95442449+ThesisGroup7@users.noreply.github.com>
2022-05-11 18:33:19 +0200

    inference file

--- cavaj/src/inference.py
+++ cavaj/src/inference.py
@@ -1,7 +1,22 @@
-def generateCode(ast):
+import inspect
+import javalang
+import sys
+import javalangSelect
+from copy import deepcopy
+import networkx as nx
+from pathlib import Path
+
+type_map = [i for name,i in inspect.getmembers(sys.modules[javalang.tree.__name__]) if inspect.isclass(i)] 
+# print(type_map)
+
+def generateCode(ast, file):
     type_map_code = []
+    codeConv = []
     code = []
     type_map_genCode = list(nx.dfs_preorder_nodes(ast, source=0))
+    for item in type_map_genCode:
+        codeline = code.append(type_map[item])
+        code.append(javalangSelect.ret_string(codeline))
     return code 
 
 
